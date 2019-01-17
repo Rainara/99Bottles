f02030763109b320f021b162188cb134b12cc454
Rainara Araujo <rmaraujo@thoughtworks.com>
2019-01-17 10:09:28 -0200

    Refactor tests and split displaySong Method into more concise and independent methods (verse and rangeVerse) to apply single responsability for eacach one

--- src/BottlesSong.java
+++ src/BottlesSong.java
@@ -1,36 +1,37 @@
 public class BottlesSong {
 
-    private int numberOfBottles;
-    private String song = "";
-
     private static final String noBottles = "No more bottles of beer on the wall, no more bottles of beer. \n" +
             "Go to the store and buy some more, 99 bottles of beer on the wall.";
     private static final String oneBottle = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-            "Take one down and pass it around, no more bottles of beer on the wall.\n";
+            "Take one down and pass it around, no more bottles of beer on the wall.";
+    private static final String twoBottles = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+            "Take one down and pass it around, 1 bottle of beer on the wall.";
 
-    public BottlesSong(int numberOfBottles) {
-        this.numberOfBottles = numberOfBottles;
+    public BottlesSong() {
     }
 
-    private String defineSingularOrPlural() {
-        return ((numberOfBottles-1) > 1) ? "bottles" : "bottle";
+    public String verse(int numberOfBottles) {
+        switch (numberOfBottles) {
+            case 0 : return noBottles;
+            case 1 : return oneBottle;
+            case 2 : return twoBottles;
+            default: return numberOfBottles + " bottles of beer on the wall, "+ numberOfBottles +" bottles of beer.\n"
+                    + "Take one down and pass it around, "+ (numberOfBottles -1) +" bottles of beer on the wall.";
+        }
+
+    }
+
+    public String rangeVerses(int initial, int ending) {
+        String rangeVerse="";
+        for (int i = initial; i>ending; i--){
+            rangeVerse = rangeVerse + verse(i) + "\n";
+        }
+        return rangeVerse + verse(ending);
     }
 
     public String displaySong() {
-        if(numberOfBottles==0){
-            return noBottles;
-        }
-        while (numberOfBottles>1){
-            song = song + numberOfBottles + " bottles of beer on the wall, " + numberOfBottles + " bottles of beer.\n" +
-                    "Take one down and pass it around, " + (numberOfBottles -1) + " " + defineSingularOrPlural()
-                    + " of beer on the wall.\n";
-            numberOfBottles--;
-        }
-        return song + oneBottle + noBottles;
+        return rangeVerses(99,0);
     }
 
-    public static void main(String[] args) {
-        BottlesSong bottlesSong = new BottlesSong(99);
-        System.out.println(bottlesSong.displaySong());
-    }
+
 }
